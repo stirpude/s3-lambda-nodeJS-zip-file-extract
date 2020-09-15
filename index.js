8e007c515bec0a13d7f730c6ccad7c82a3253a48
stirpude <31862506+stirpude@users.noreply.github.com>
2020-09-15 14:42:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var s = new s3Unzip({
-    bucket: "test-bucket-in-s3",
-    file: "Companies.zip",
-    deleteOnSuccess: true,
-    verbose: false
-  }, function(err, success){
-    if (err) console.error(err);
-    else console.log(success);
-  });
+const S3Unzip = require('s3-unzip'); 
+
+
+
+  exports.handler = function(event, context, callback) { 
+  
+  	  const bucketname = event.Records[0].s3.bucket.name;
+      const filename = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+	new S3Unzip({ 
+	
+		bucket: bucketname, 
+		file: filename , 
+	 	verbose: true, 
+ 	}, function(err, success) { 
+		if (err) { 
+			callback(err); 
+		} else { 
+			callback(null); 
+		} 
+ 	}); 
+} 
